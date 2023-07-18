94fa015e014eb4c150bb792284d67462c371ffee
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2023-07-18 07:31:21 +0000

    chore: improve tests

--- tests/test_repl_read.py
+++ tests/test_repl_read.py
@@ -1,7 +1,7 @@
 import sqlparse
 import pytest
 
-from pykli.tokens import initialize_sqlparse, SessionVar
+from pykli.tokens import initialize_sqlparse, SessionVar, KRunScript
 from pykli.repl_read import tokenize_ksql
 
 
@@ -9,10 +9,24 @@ from pykli.repl_read import tokenize_ksql
 def ksqldb_mock():
     initialize_sqlparse()
 
+# TODO implement proper test
+@pytest.mark.parametrize("ksql", [
+    ("  RUN   SCRIPT   '/qwe/asd/asd.ksql';"),
+    ("run   SCRIPT   '/qwe/asd/asd.ksql'"),
+])
+def test_tokenize_ksql_run_script(ksql):
+    stmts = sqlparse.parse(ksql)
+    assert len(stmts) == 1
+
+    t = stmts[0].token_first()
+    assert t.ttype == KRunScript
+
 
 @pytest.mark.parametrize("ksql", [
     ("define my_var = 'my_val';"),
-    ("  define   my_var  =  'my_val';"),
+    ("  define   my_var  =  'my_val' "),
+    ("DEFINE my_var = 'my_val';"),
+    ("  DEFINE   my_var  =  'my_val' "),
 ])
 def test_tokenize_ksql_define(ksql):
     stmt = sqlparse.parse(ksql)[0]
@@ -24,7 +38,8 @@ def test_tokenize_ksql_define(ksql):
 
 @pytest.mark.parametrize("ksql", [
     ("undefine my_var;"),
-    ("  undefine   my_var ; "),
+    ("  undefine   my_var "),
+    ("  UNDEFINE  my_var"),
 ])
 def test_tokenize_ksql_undefine(ksql):
     stmt = sqlparse.parse(ksql)[0]
